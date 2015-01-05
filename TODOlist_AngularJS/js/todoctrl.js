96aa6cadafc24d91775b08a260f1dfff4356ae0e
unknown <weronika.zietek@gmail.com>
2015-01-05 18:37:20 +0100

    ToDo list AngularJS

--- TODOlist_AngularJS/js/todoctrl.js
+++ TODOlist_AngularJS/js/todoctrl.js
@@ -1,2 +1,26 @@
 angular.module('myapp', []).controller('TodoCtrl', function ($scope) {
+    $scope.elements = [];
+    $scope.inputValue = '';
+    $scope.active = 0;
+
+    $scope.addNew = function () {
+        if (!$scope.inputValue) {
+            return;
+        }
+        $scope.elements.push({
+            value: $scope.inputValue,
+            status: true
+        });
+        $scope.inputValue = '';
+    };
+
+    $scope.check = function (item) {
+        if ($scope.elements[item].status === true) {
+            $scope.elements[item].status = false;
+            $scope.active = $scope.active + 1;
+        } else {
+            $scope.elements[item].status = true;
+            $scope.active = $scope.active - 1;
+        }
+    }
 });
